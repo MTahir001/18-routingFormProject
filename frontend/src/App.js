6d8c82561d9982fa0694f5ec53a74c087bc659a4
MTahir01 <tahirkadgaonkar01@gmail.com>
2024-03-20 14:32:43 +0530

    Dynamic paths, list of Links and more

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -27,6 +27,7 @@ import HomePage from "./pages/HomePage";
 import EventsPage from "./pages/EventsPage";
 import EventDetailPage from "./pages/EventDetailPage";
 import NewEventPage from "./pages/NewEventPage";
+import EventsRootLayout from "./pages/EventsRoot";
 
 const router = createBrowserRouter([
   {
@@ -34,10 +35,16 @@ const router = createBrowserRouter([
     element: <RootLayout />,
     children: [
       { index: true, element: <HomePage /> },
-      { path: "events", element: <EventsPage /> },
-      { path: "events/:eventId", element: <EventDetailPage /> },
-      { path: "events/new", element: <NewEventPage /> },
-      { path: "events/:eventId/edit", element: <EditEventPage /> },
+      {
+        path: "events",
+        element: <EventsRootLayout />,
+        children: [
+          { index: true, element: <EventsPage /> },
+          { path: ":eventId", element: <EventDetailPage /> },
+          { path: "new", element: <NewEventPage /> },
+          { path: ":eventId/edit", element: <EditEventPage /> },
+        ],
+      },
     ],
   },
 ]);
--- frontend/src/pages/EventsPage.js
+++ frontend/src/pages/EventsPage.js
@@ -1,3 +1,19 @@
+import { Link } from "react-router-dom";
 export default function EventsPage() {
-  return <h1>EventsPage</h1>;
+  const DUMMY_EVENTS = [
+    { id: "e1", title: "Tea" },
+    { id: "e2", title: "Coffee" },
+  ];
+  return (
+    <>
+      <h1>EventsPage</h1>
+      <ul>
+        {DUMMY_EVENTS.map((e) => (
+          <li key={e.id}>
+            <Link to={e.id}>{e.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
